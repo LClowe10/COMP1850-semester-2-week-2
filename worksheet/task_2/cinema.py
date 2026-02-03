b0b7f70fff6ebf5d82ad6872901a65cbac504041
Lucas Parbott Clowe <cmnp0658@leeds.ac.uk>
2026-02-03 22:47:39 +0000

    Started worksheet

--- worksheet/task_2/cinema.py
+++ worksheet/task_2/cinema.py
@@ -18,7 +18,11 @@ def customer_tickets(conn, customer_id):
     Include only tickets purchased by the given customer_id.
     Order results by film title alphabetically.
     """
-    pass
+    query = '''
+            SELECT title AS file_title, screen, price, customer_id
+            FROM films
+            JOIN 
+            '''
 
 
 def screening_sales(conn):
@@ -29,7 +33,16 @@ def screening_sales(conn):
     Include all screenings, even if tickets_sold is 0.
     Order results by tickets_sold descending.
     """
-    pass
+    query = '''
+            SELECT s.screening_id, title AS film_title, COUNT(*) AS tickets_sold
+            FROM screenings s
+            LEFT JOIN tickets t
+            ON s.screening_id = t.screening_id
+            LEFT JOIN films f
+            ON s.film_id = f.film_id
+            GROUP BY s.screening_id
+            ORDER BY tickets_sold DESC;
+            '''
 
 
 def top_customers_by_spend(conn, limit):
@@ -42,4 +55,5 @@ def top_customers_by_spend(conn, limit):
     Order by total_spent descending.
     Limit the number of rows returned to `limit`.
     """
-    pass
+    query = '''
+            '''
