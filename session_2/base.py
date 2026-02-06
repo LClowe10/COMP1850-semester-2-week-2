e7c3af4f791adb802013a2693713de63a9ffe27e
Lucas Parbott Clowe <cmnp0658@leeds.ac.uk>
2026-02-06 12:56:58 +0000

    session 2

--- session_2/base.py
+++ session_2/base.py
@@ -12,6 +12,39 @@ def get_connection(db_path="orders.db"):
     conn.row_factory = sqlite3.Row
     return conn
 
+def top_five_spenders(conn):
+    query = '''
+            SELECT c.customer_id, first_name, last_name, total_amount
+            FROM customers c
+            LEFT JOIN orders o
+            ON c.customer_id = o.customer_id
+            ORDER BY total_amount DESC
+            LIMIT 5;
+            '''
+    cursor = conn.execute(query)
+    results = cursor.fetchall()
+
+    return results
+
+def orders_per_category(conn):
+    query = '''
+            SELECT category, COUNT(*) AS amount
+            FROM products p
+            LEFT JOIN order_items oi
+            ON p.product_id = oi.product_id
+            GROUP BY category
+            ORDER BY amount;
+            '''
+    cursor = conn.execute(query)
+    results = cursor.fetchall()
+
+    return results
+
+def top_ten_products():
+    query = '''
+            SELECT p.product_id, p.name, 
+            '''
+
 def main():
 
     db = get_connection()
